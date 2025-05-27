dabf9c457cf0073caf00407822b27d275e71ddc8
TG-AUFC <tomas.greco@aufc.com.au>
2025-05-27 14:49:50 +0930

    Add files via upload

--- gsheet.py
+++ gsheet.py
@@ -1,10 +1,16 @@
 import gspread
 import pandas as pd
+import json
+import os
 from oauth2client.service_account import ServiceAccountCredentials
 
 def connect_sheet(sheet_name):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    
+    # Cargar credenciales desde los secrets
+    creds_dict = json.loads(os.environ["GOOGLE_CREDS"])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
     client = gspread.authorize(creds)
     sheet = client.open(sheet_name)
     return sheet
